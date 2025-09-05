fba520981f9b32a92046a57b96a9006fede773ce
Peggy <peggy.souttre@fabeon.fr>
2025-09-05 16:25:48 +0200

    projet ok

--- frontend/streamlit.py
+++ frontend/streamlit.py
@@ -19,14 +19,14 @@ photo = st.camera_input("Prenez une photo avec la webcam")
 if st.button("Connexion") and username and password and photo:
 
     # Appel API login pour récupérer user_id
-    login_url = "http://api_flask:8000/login"                                              # URL du service API
+    login_url = "http://api:8000/login"                                              # URL du service API
     login_data = {"username": username, "password": password}                                # Données à envoyer   
     login_resp = requests.post(login_url, json=login_data)                                   # Requête POST pour login   
 
     if login_resp.status_code == 200:
         # Utilisateur connu
         user_id = login_resp.json().get("user_id")
-        verify_url = "http://api_flask:8000/verify_face"
+        verify_url = "http://api:8000/verify_face"
         files = {"file": (f"{username}.jpg", photo.getvalue(), "image/jpeg")}             
         data = {"user_id": user_id}
         ver_resp = requests.post(verify_url, files=files, data=data)                       # Requête POST pour vérification
@@ -40,7 +40,7 @@ if st.button("Connexion") and username and password and photo:
     else:
         # Utilisateur inconnu → création + enregistrement
         st.info("Utilisateur inconnu → création d'un compte temporaire.")
-        register_url = "http://api_flask:8000/register_face"
+        register_url = "http://api:8000/register_face"
         files = {"file": (f"{username}.jpg", photo.getvalue(), "image/jpeg")}
         data = {"username": username, "password": password}   
 
